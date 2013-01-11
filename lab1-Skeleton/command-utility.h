cecddd950cf465adc0a1da3e66471f03c6810498
bngo92@gmail.com <bryan@bryan-laptop.(none)>
2013-01-11 14:32:49 -0800

    tests successful

--- lab1-Skeleton/command-utility.h
+++
@@ -1,8 +0,0 @@
-#include "command.h"
-
-void get_command(command_t c, char *s, int n);
-int get_sequence(command_t c, char *s, int n);
-int get_subshell(command_t c, char *s, int n);
-int get_andor(command_t c, char *s, int n);
-int get_pipeline(command_t c, char *s, int n);
-void get_simple(command_t c, char *s, int n);
--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -12,14 +12,357 @@
 #include <string.h>
 #include <stdbool.h>
 
+/* Make tree */
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int line_number = 0;
+
+void get_command(command_t c, char *s, int n);
+int get_sequence(command_t c, char *s, int n);
+int get_subshell(command_t c, char *s, int n);
+int get_andor(command_t c, char *s, int n);
+int get_pipeline(command_t c, char *s, int n);
+void get_simple(command_t c, char *s, int n);
+
+char *strdcpy(const char *src, size_t n)
+{
+  char* buffer = (char*) checked_malloc(n+1);
+  strncpy(buffer, src, n);
+  buffer[n]='\0';
+  return buffer;
+}
+
+int iswordchar(char c)
+{
+  if (isalpha(c) || isdigit(c))
+    return 1;
+  switch (c) {
+  case '!':
+  case '%':
+  case '+':
+  case ',':
+  case '-':
+  case '.':
+  case '/':
+  case ':':
+  case '@':
+  case '^':
+  case '_':
+    return 1;
+  default:
+    return 0;
+  }
+}
+
+/*
+@postcondition  *s is the beginning of the word, or the next token char if 
+                no word is found
+@return length of word, or 0 if no word is found
+*/
+int get_next_word(char **s)
+{
+  char *ret = *s;
+  //nullbyte at end of buffer
+  while (!iswordchar(*ret)) {
+    if (isspace(*ret)) {
+      ret++;
+    } else {
+      *s = ret;
+      return 0;
+    }
+  }
+
+  int i = 0;
+  while (iswordchar(ret[i])) {
+    i++;
+  }
+  *s = ret;
+  return i;
+}
+
+/*
+@return pointer to next char of s that is either *s1 or *s2
+*/
+char* find_either(char *s, char *s1, char *s2)
+{
+  char *ret1 = strstr(s, s1);
+  char *ret2 = strstr(s, s2);
+
+  if (ret1 == NULL && ret2 == NULL)
+    return NULL;
+  else if (ret1 != NULL && ret2 != NULL)
+    return (ret1 < ret2) ? ret1 : ret2;
+  else if (ret1 != NULL)
+    return ret1;
+  else
+    return ret2;
+}
+
+/*
+@precondition s is pointing to a left parenthesis
+@return pointer to matching parenthesis
+*/
+char* find_matching_paren(char* s)
+{
+  s++; // skip starting paren
+
+  int depth = 1;
+  while (depth != 0) {
+    s += strcspn(s, "()");
+    if (*s == '\0') {
+      error(1, 0, "line number: %d", line_number);
+    } else if (*s == '(') {
+      depth++;
+      s++;
+    } else {
+      depth--;
+      s++;
+    }
+  }
+  return s;
+}
+
+/*
+creates tree of commands parsing s, with c as the head command
+@param  n   size of *s
+*/
+void get_command(command_t c, char *s, int n) 
+{
+  if (get_subshell(c, s, n))
+    return;
+  
+  if (get_sequence(c, s, n))
+    return;
+
+  char *comment = strchr(s, '#');
+  if (comment != NULL) {
+      *comment = '\0';
+      n = comment - s;
+  }
+
+  if (get_andor(c, s, n))
+    return;
+
+  if (get_pipeline(c, s, n))
+    return;
+
+  get_simple(c, s, n);
+}
+
+int get_sequence(command_t c, char *s, int n)
+{
+  while(isspace(*s))
+  { 
+    s++;
+    n--;
+  }
+  while(isspace(s[n-1]))
+  {
+    n--;
+  }
+
+  char *buffer = strdcpy(s, n);
+  
+  char *found = buffer + strcspn(buffer, "(;\n");
+  while (1) {
+    if (*found == '\0')
+    {
+      free(buffer);
+      return 0;
+    } else if (*found == '(') {
+      found = find_matching_paren(found);
+    } else {
+      if (*found == '\n')
+        line_number++;
+      break;
+    }
+    found += strcspn(found, "(;\n");
+  }
+  
+  c->type = SEQUENCE_COMMAND;
+  c->status = -1;
+  c->input = NULL;
+  c->output = NULL;
+  
+  c->u.command[0] = (command_t) checked_malloc(sizeof(struct command));
+  get_command(c->u.command[0], buffer, (found - buffer));
+  c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
+  get_command(c->u.command[1], found + 1, n - (found - buffer));
+
+  free(buffer);
+  return 1;
+}
+
+int get_subshell(command_t c, char *s, int n)
+{
+  // strip leading and trailing whitespace
+  while (isspace(s[0])) {
+    s++;
+    n--;
+  }
+  while (isspace(s[n-1])) {
+    n--;
+  }
+
+  if (*s != '(') {
+    return 0;
+  }
+
+  char *buffer = strdcpy(s, n);
+  if(find_matching_paren(buffer) - buffer != n-1)
+  {
+    free(buffer);
+    return 0;
+  }
+
+  c->type = SUBSHELL_COMMAND;
+  c->status = -1;
+  c->input = NULL;
+  c->output = NULL;
+  
+  c->u.subshell_command = (command_t) checked_malloc(sizeof(struct command*));
+  get_command(c->u.subshell_command, buffer + 1, n - 2);
+
+  free(buffer);
+  return 1;
+}
+
+int get_andor(command_t c, char *s, int n) {
+  char *buffer = strdcpy(s, n);
+
+  char *found = find_either(buffer, "&&", "||");
+  if(found == NULL)
+  {
+    free(buffer);
+    return 0;
+  }
+  
+  if(*found == '&')
+    c->type = AND_COMMAND;
+  else if(*found == '|')
+    c->type = OR_COMMAND;
+  else
+    error(1, 0, "line number: %d", line_number);
+
+  c->status = -1; 
+  c->input = NULL;
+  c->output = NULL;
+
+  c->u.command[0] = (command_t) checked_malloc(sizeof(struct command)); 
+  get_command(c->u.command[0], buffer, found-buffer);
+  c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
+  get_command(c->u.command[1], found+2, n-(found+2-buffer));
+
+  //free(buffer);
+  return 1;
+}
 
 
+int get_pipeline(command_t c, char *s, int n) {
+  char* buffer = strdcpy(s, n);
+
+  char* found = strchr(buffer, '|');
+  if(found==NULL)
+  {
+    free(buffer);
+    return 0;
+  }
+
+  c->type = PIPE_COMMAND;
+  c->status = -1; 
+  c->input = NULL;
+  c->output = NULL;
+
+  c->u.command[0] = (command_t) checked_malloc(sizeof(struct command)); 
+  get_command(c->u.command[0], buffer, (found - buffer)); 
+  c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
+  get_command(c->u.command[1], found+1, n-(found - buffer));
+
+  free(buffer);
+  return 1;
+
+}
+
+
+void get_simple(command_t c, char *s, int n) 
+{
+  char *buffer = strdcpy(s, n);
+  char* save_ptr = buffer;
+
+  c->type = SIMPLE_COMMAND;
+  c->status = -1;
+  c->input = NULL;
+  c->output = NULL;
+
+  size_t size = 0;
+  int word_count = 0;
+  c->u.word = (char **) checked_malloc(sizeof(char *));
+  int i=0;
+  while(1) {
+    i = get_next_word(&buffer);
+    if (i < 1) {
+      if (word_count == 0) 
+        error(1, 0, "line number: %d", line_number);
+      if (*buffer == '\0' || *buffer == '<' || *buffer == '>')
+        break;
+      error(1, 0, "line number: %d", line_number);
+    }
+    
+    word_count++;
+    size = size + sizeof(char *);
+    c->u.word = (char **) checked_realloc(c->u.word, size);
+    c->u.word[word_count-1] = (char *) checked_malloc((i + 1) * sizeof(char));
+    strncpy(c->u.word[word_count-1], buffer, i);
+    
+    c->u.word[word_count-1][i] = '\0';
+    buffer = buffer+i;
+  
+  }
+  if (*buffer == '<') {
+    buffer++;
+    i = get_next_word(&buffer);
+    if (i < 1)
+      error(1, 0, "line number: %d", line_number);
+
+    c->input = (char *) checked_malloc((i + 1) * sizeof(char));
+    strncpy(c->input, buffer, i);
+    c->input[n] = '\0';
+    buffer = buffer+i;
+    while (isspace(*buffer))
+      buffer++;
+  }
+
+  if (*buffer == '>') {
+    buffer++;
+    int i = get_next_word(&buffer);
+    if (i < 1)
+      error(1, 0, "line number: %d", line_number);
+
+    c->input = (char *) checked_malloc((i + 1) * sizeof(char));
+    strncpy(c->input, buffer, i);
+    c->input = '\0';
+    buffer = buffer+i;
+  }
+
+  c->u.word[word_count] = NULL;
+
+  while (isspace(*buffer))
+    buffer++;
+
+  if (*buffer != '\0')
+    error(1, 0, "line number: %d", line_number);
+
+  free(save_ptr);
+}
        
 /* FIXME: Define the type 'struct command_stream' here.  This should complete the incomplete type declaration in command.h.  */
 
 struct command_stream
 {
     command_t root;
+    int i;
 };
 
 
@@ -53,9 +396,14 @@ make_command_stream (int (*get_next_byte) (void *),
     size++;
   }
   buf[pos] = '\0'; 
-  //pos++
+  int bt = pos-1;
+  while (isspace(buf[bt]) || buf[bt] == ';') {
+    buf[bt] = '\0';
+    bt--;
+  }
 
-  ret_stream->root = (command_t) checked_malloc(sizeof(command_t));
+  ret_stream->i = 0;
+  ret_stream->root = (command_t) checked_malloc(sizeof(struct command));
   get_command(ret_stream->root, buf, pos );
 
 
@@ -65,5 +413,9 @@ make_command_stream (int (*get_next_byte) (void *),
 command_t
 read_command_stream (command_stream_t s)
 {
-  return s->root;
+  if (s->i == 0) {
+    s->i++;
+    return s->root;
+  }
+  return NULL;
 }
