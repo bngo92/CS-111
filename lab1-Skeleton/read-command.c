5caf2c4a6ee1a46ca8bfe3e8eb21ecee3bc25157
= <=>
2013-01-15 19:56:21 -0800

    Tests successful

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -2,15 +2,13 @@
 #include "command.h"
 #include "command-internals.h"
 
+#include <ctype.h>
 #include <error.h>
-
-#include "alloc.h"
-#include <ctype.h>
-
-/* Make tree */
-#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "alloc.h"
 
 enum token_type
 {
@@ -22,7 +20,8 @@ enum token_type
   LEFT_PAREN,
   RIGHT_PAREN,
   LEFT_BRACKET,
-  RIGHT_BRACKET
+  RIGHT_BRACKET,
+  END
 };
 
 struct token
@@ -49,19 +48,30 @@ struct token *find_token(struct token *a, enum token_type t, int n)
   return NULL;
 }
 
-void create_token(struct token *t, enum token_type type, int line_number)
+struct token *find_last_token(struct token *a, enum token_type t, int n)
 {
-  t = (struct token *) checked_malloc(sizeof(struct token));
-  t->type = type;
-  t->line_number = line_number;
+  int i = n-1;
+  for (; i >= 0; i--)
+    if (a[i].type == t)
+      return a + i;
+  return NULL;
 }
 
-void create_word_token(struct token *t, const char* s, int n, int line_number)
+struct token create_token(enum token_type type, int line_number)
 {
-  t = (struct token *) checked_malloc(sizeof(struct token));
-  t->type = WORD;
-  t->word = strdcpy(s, n);
-  t->line_number = line_number;
+  struct token ret;
+  ret.type = type;
+  ret.line_number = line_number;
+  return ret;
+}
+
+struct token create_word_token(const char* s, int n, int line_number)
+{
+  struct token ret;
+  ret.type = WORD;
+  ret.word = strdcpy(s, n);
+  ret.line_number = line_number;
+  return ret;
 }
 
 void get_command(command_t c, struct token *s, int n);
@@ -163,7 +173,7 @@ int get_sequence(command_t c, struct token *s, int n)
   c->u.command[0] = (command_t) checked_malloc(sizeof(struct command));
   get_command(c->u.command[0], s, (found - s));
   c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
-  get_command(c->u.command[1], found + 1, n - (found - s));
+  get_command(c->u.command[1], found + 1, n - (found+1- s));
   return 1;
 }
 
@@ -188,12 +198,12 @@ int get_subshell(command_t c, struct token *s, int n)
 
 int get_andor(command_t c, struct token *s, int n) {
   struct token *found;
-  struct token *found1 = find_token(s, AND, n);
-  struct token *found2 = find_token(s, OR, n);
+  struct token *found1 = find_last_token(s, AND, n);
+  struct token *found2 = find_last_token(s, OR, n);
   if (found1 == NULL && found2 == NULL)
     return 0;
   else if (found1 != NULL && found2 != NULL)
-    found = (found1 < found2) ? found1 : found2;
+    found = (found1 > found2) ? found1 : found2;
   else if (found1 != NULL)
     found = found1;
   else
@@ -213,7 +223,7 @@ int get_andor(command_t c, struct token *s, int n) {
   c->u.command[0] = (command_t) checked_malloc(sizeof(struct command)); 
   get_command(c->u.command[0], s, found-s);
   c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
-  get_command(c->u.command[1], found+2, n-(found+2-s));
+  get_command(c->u.command[1], found+1, n-(found+1-s));
   return 1;
 }
 
@@ -230,7 +240,7 @@ int get_pipeline(command_t c, struct token *s, int n) {
   c->u.command[0] = (command_t) checked_malloc(sizeof(struct command)); 
   get_command(c->u.command[0], s, (found - s)); 
   c->u.command[1] = (command_t) checked_malloc(sizeof(struct command));
-  get_command(c->u.command[1], found+1, n-(found - s));
+  get_command(c->u.command[1], found+1, n-(found+1 - s));
 
   return 1;
 
@@ -238,33 +248,42 @@ int get_pipeline(command_t c, struct token *s, int n) {
 
 void get_simple(command_t c, struct token *s, int n) 
 {
+  if (n == 0)
+    error(1, 0, "line number: %d", 42);
+
   c->type = SIMPLE_COMMAND;
   c->status = -1;
   c->input = NULL;
   c->output = NULL;
 
-  size_t size = 10;
+  size_t size = 1 * sizeof(char *);
   size_t word_count = 0;
   c->u.word = (char **) checked_malloc(sizeof(char *));
   int i = 0;
+  if (s[i].type != WORD)
+    error(1, 0, "line number: %d", s[i].line_number);
   while(s[i].type == WORD) {
     c->u.word[word_count] = s[i].word;
     word_count++;
-    if (word_count == size)
+    if (word_count * sizeof(char *) == size)
       c->u.word = (char **) checked_grow_alloc(c->u.word, &size);
     i++;
   }
+  c->u.word[word_count] = NULL;
+
   if (s[i].type == LEFT_BRACKET) {
     i++;
     if (s[i].type != WORD)
       error(1, 0, "line number: %d", s[i].line_number);
     c->input = s[i].word;
+    i++;
   }
   if (s[i].type == RIGHT_BRACKET) {
     i++;
     if (s[i].type != WORD)
       error(1, 0, "line number: %d", s[i].line_number);
     c->output = s[i].word;
+    i++;
   }
   if (i != n)
     error(1, 0, "line number: %d", s[i].line_number);
@@ -274,7 +293,6 @@ void get_simple(command_t c, struct token *s, int n)
 struct command_stream
 {
     command_t root;
-    int i;
 };
 
 
@@ -286,84 +304,102 @@ make_command_stream (int (*get_next_byte) (void *),
 
   size_t buffer_pos = 0;
   size_t buffer_size = 80;
-  char *buffer = (char *) checked_malloc(buffer_size * sizeof(char));
+  char *buffer = (char *) checked_malloc(buffer_size);
   memset(buffer, 0, buffer_size * sizeof(char));
 
   size_t token_pos = 0;
-  size_t token_size = 10;
-  struct token *tokens = (struct token *) checked_malloc(token_size * sizeof(struct token *));
+  size_t token_size = 10 * sizeof(struct token);
+  struct token *tokens = (struct token *) checked_malloc(token_size);
   
   int line_number = 1;
   int newline = 0;
   char nextchar = (char) get_next_byte(get_next_byte_argument);
   while(nextchar != EOF) {
     //update buffer size as necessary
-    if (token_pos == token_size) {
+    if (token_pos * sizeof(struct token) == token_size) {
       tokens = checked_grow_alloc(tokens, &token_size);
     }
+    if (nextchar=='#') {
+      while(nextchar!='\n')
+        nextchar = get_next_byte(get_next_byte_argument);
+    } 
     if (isspace(nextchar)) {
-      if (nextchar != '\n') {
-        newline = 0;
-      } else { enum token_type prev_token = tokens[token_pos-1].type;
-        if (prev_token == SEMICOLON || prev_token == AND ||
-            prev_token == PIPE || prev_token == LEFT_PAREN ||
-            prev_token == RIGHT_PAREN) {
-        } else if (prev_token == LEFT_BRACKET || prev_token == RIGHT_BRACKET) {
-          error(1, 0, "line number: %d", line_number);
-        } else {
-          create_token(&tokens[token_pos], SEMICOLON, line_number);
-          token_pos++;
-        }
-        line_number++;
-        newline = 1;
-      } 
       if (buffer_pos != 0) {
-        create_word_token(&tokens[token_pos], buffer, buffer_pos, line_number);
+        tokens[token_pos] = create_word_token(buffer, buffer_pos, line_number);
         token_pos++;
 
         memset(buffer, 0, buffer_pos);
         buffer_pos = 0;
       }
-    } if (iswordchar(nextchar)) {
+      if (nextchar != '\n') {
+        newline = 0;
+      } else { enum token_type prev_token = tokens[token_pos-1].type;
+        if (prev_token == SEMICOLON || prev_token == AND || prev_token == OR ||
+            prev_token == PIPE || prev_token == LEFT_PAREN ||
+            prev_token == RIGHT_PAREN) {
+        } else if (prev_token == LEFT_BRACKET || prev_token == RIGHT_BRACKET) {
+          error(1, 0, "line number: %d", line_number);
+        } else {
+          tokens[token_pos] = create_token(SEMICOLON, line_number);
+          token_pos++;
+        }
+        line_number++;
+        newline = 1;
+      } 
+    } else if (iswordchar(nextchar)) {
       buffer[buffer_pos] = nextchar;
-      buffer[buffer_pos+1] = '\0';
       buffer_pos++;
       if (buffer_pos == buffer_size) {
         buffer = (char *) checked_grow_alloc(buffer, &buffer_size);
       }
       newline = 0;
     } else if (isToken(nextchar)) {
+      if (buffer_pos != 0) {
+        tokens[token_pos] = create_word_token(buffer, buffer_pos, line_number);
+        token_pos++;
+
+        memset(buffer, 0, buffer_pos);
+        buffer_pos = 0;
+
+        if (token_pos * sizeof(struct token) == token_size) {
+          tokens = checked_grow_alloc(tokens, &token_size);
+        }
+      }
       if (tokens[token_pos-1].type != WORD)
         error(1, 0, "line number: %d", line_number);
       if (nextchar == '(') {
-        create_token(&tokens[token_pos], LEFT_PAREN, line_number);
+        tokens[token_pos] = create_token(LEFT_PAREN, line_number);
         token_pos++;
       } else if (nextchar == ')') {
-        create_token(&tokens[token_pos], RIGHT_PAREN, line_number);
+        tokens[token_pos] = create_token(RIGHT_PAREN, line_number);
         token_pos++;
       } else if (newline) {
         error(1, 0, "line number: %d", line_number);
       } else if (nextchar == ';') {
-        create_token(&tokens[token_pos], SEMICOLON, line_number);
+        tokens[token_pos] = create_token(SEMICOLON, line_number);
         token_pos++;
       } else if (nextchar == '&') {
         nextchar = get_next_byte(get_next_byte_argument);
         if (nextchar != '&') {
           error(1, 0, "line number: %d", line_number);
         }
-        create_token(&tokens[token_pos], AND, line_number);
+        tokens[token_pos] = create_token(AND, line_number);
+        token_pos++;
       } else if (nextchar == '|') {
-        if (tokens[token_pos-1].type == PIPE) {
-          tokens[token_pos-1].type = OR;
-        } else {
-          create_token(&tokens[token_pos], PIPE, line_number);
+        nextchar = get_next_byte(get_next_byte_argument);
+        if (nextchar == '|') {
+          tokens[token_pos] = create_token(OR, line_number);
           token_pos++;
+        } else {
+          tokens[token_pos] = create_token(PIPE, line_number);
+          token_pos++;
+          continue;
         }
       } else if (nextchar == '<') {
-        create_token(&tokens[token_pos], LEFT_BRACKET, line_number);
+        tokens[token_pos] = create_token(LEFT_BRACKET, line_number);
         token_pos++;
       } else if (nextchar == '>') {
-        create_token(&tokens[token_pos], RIGHT_BRACKET, line_number);
+        tokens[token_pos] = create_token(RIGHT_BRACKET, line_number);
         token_pos++;
       } else {
         error(1, 0, "line number: %d", line_number);
@@ -375,7 +411,16 @@ make_command_stream (int (*get_next_byte) (void *),
     nextchar = get_next_byte(get_next_byte_argument);
   }
 
-  ret_stream->i = 0;
+  if (buffer_pos != 0) {
+    tokens[token_pos] = create_word_token(buffer, buffer_pos, line_number);
+    token_pos++;
+  }
+  // optional following semicolon
+  if (tokens[token_pos-1].type == SEMICOLON) {
+    token_pos--;
+  }
+  if (token_pos == 0)
+    error(1, 0, "line number: %d", line_number);
   ret_stream->root = (command_t) checked_malloc(sizeof(struct command));
   get_command(ret_stream->root, tokens, token_pos);
   return ret_stream;
@@ -384,9 +429,17 @@ make_command_stream (int (*get_next_byte) (void *),
 command_t
 read_command_stream (command_stream_t s)
 {
-  if (s->i == 0) {
-    s->i++;
-    return s->root;
+  if (s == NULL || s->root == NULL)
+    return NULL;
+
+  command_t ret;
+  if (s->root->type != SEQUENCE_COMMAND) {
+    ret = s->root;
+    s->root = NULL;
+  } else {
+    ret = s->root->u.command[0];
+    s->root = s->root->u.command[1];
   }
-  return NULL;
+
+  return ret;
 }
