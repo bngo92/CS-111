91cdbf8f7fe4573465092c940117ad035346275b
Rachel Fang <rfang@rfang-laptop.(none)>
2013-01-31 17:01:05 -0800

    1C version 1. segfaults known

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -164,3 +164,174 @@ execute_command (command_t c, bool time_travel)
 {
   command_print(c);
 }
+
+/****************************************************************************
+TIME TRAVEL VERSION
+***************************************************************************/
+enum p_status
+{
+    NOT_RAN,
+    RUNNING,
+    RAN
+};
+
+struct d_pair
+{
+    command_t c; 
+    char **input;
+    size_t input_size;
+    size_t input_max;
+    char **output;
+    size_t output_size;
+    size_t output_max;
+    enum p_status ran; //bool
+};
+
+
+void fill(struct d_pair *pair, command_t c)
+{
+    if (c->input) {
+        pair->input[pair->input_size] = c->input;
+        pair->input_size++;
+        if (pair->input_size * sizeof(char *) >= pair->input_max/ 2)
+            pair->input = checked_grow_alloc(pair->input, &pair->input_max);
+    }
+
+    char **temp = c->u.word;
+    while (temp != NULL && *temp!=NULL) {
+        pair->input[pair->input_size] = *temp;
+        pair->input_size++;
+        if (pair->input_size * sizeof(char *) >= pair->input_max/ 2)
+            pair->input = checked_grow_alloc(pair->input, &pair->input_max);
+		temp++;
+    }
+
+	if (c->output) {
+        pair->output[pair->output_size] = c->output;
+        pair->output_size++;
+        if (pair->output_size * sizeof(char *) >= pair->output_max/ 2)
+            pair->output = checked_grow_alloc(pair->output, &pair->output_max);
+    }
+
+    switch (c->type) {
+    case AND_COMMAND:
+    case OR_COMMAND:
+    case PIPE_COMMAND:
+    case SEQUENCE_COMMAND:
+        fill(pair, c->u.command[0]);
+        fill(pair, c->u.command[1]);
+        break;
+    case SUBSHELL_COMMAND:
+        fill(pair, c->u.subshell_command);
+        break;
+    default:
+        pair->input[pair->input_size] = NULL;
+        pair->output[pair->output_size] = NULL;
+        break;
+    }
+}
+
+int check_match(char **s1, char **s2)
+{
+    while (s1 != NULL && *s1 != NULL) {
+        char **temp = s2;
+        while (temp != NULL && *temp != NULL) {
+            if (strcmp(*s1, *temp) == 0)
+                return 1;
+            temp++;
+        }
+        s1++;
+    }
+    return 0;
+}
+
+/* 
+@precondition	function assumes time_travel has been called.
+@postcondition	all commands in command_stream have been run using time travel to maximize speed
+
+@return			status of "last command" 
+*/
+int execute_command_stream(command_stream_t command_stream) //DESIGN PROBLEM: , int max_processes)
+{
+   
+    //DESIGN PROBLEM  TODO: Implement counter for max processes; 
+    int size = 0;
+    size_t max = 10 * sizeof(struct d_pair);
+    struct d_pair* c_array = (struct d_pair*) checked_malloc( max * sizeof(struct d_pair) ); 
+	
+    //read all commands
+    //get input/output
+    command_t command = NULL; 
+    while ( ( command = read_command_stream (command_stream) ) ) {
+        struct d_pair *pair = &c_array[size];
+	    pair->c = command; 
+	    pair->ran = -1;
+	    pair->input_size=0;
+	    pair->output_size=0;
+        pair->input_max = 10 * sizeof(char *);
+        pair->output_max = 10 * sizeof(char *);
+        pair->input = (char **) checked_malloc(pair->input_max);
+        pair->output = (char **) checked_malloc(pair->output_max);
+        pair->ran = NOT_RAN;
+
+        fill(pair, pair->c);
+		size++;
+        if (size * sizeof(pair) >= max / 2)
+            c_array = checked_grow_alloc(c_array, &max);
+    }
+
+    //if null stream, return 0 as status
+    if(size==0) {
+        return 0;
+    }
+    
+    //setup pid array
+    int* pid_set = (int*) checked_malloc(size*sizeof(int)); 
+    memset(pid_set, 0, size*sizeof(int));
+    
+    int start = 0;
+    while (start != size) {
+        int i;
+        for (i = start; i < size; i++) {
+            if (c_array[i].ran != NOT_RAN)
+                continue;
+
+            // check for dependencies
+            int j;
+            int fail = 0;
+            for (j = start; j < i; j++) {
+                if (c_array[j].ran != RAN) {
+                    if (check_match(c_array[i].input, c_array[j].input) ||
+                            check_match(c_array[i].input, c_array[j].output)) {
+                        fail = 1;
+                        break;
+                    }
+                }
+            }
+            if (!fail) {
+                pid_t pid = fork();
+                if(pid == 0) {
+                    command_print(c_array[i].c);
+                    _exit(c_array[i].c->status);
+                } else {
+                    c_array[i].ran = RUNNING;
+                    pid_set[i] = pid;
+                }
+            }
+        }
+
+        for (i = start; i < size; i++) {
+            if (waitpid(pid_set[i], NULL, WNOHANG) > 0) {
+                c_array[i].ran = RAN;
+            }
+        }
+
+        // skip already ran processes
+        while (c_array[start].ran == RAN) {
+            start++;
+        }
+    }
+
+    return c_array[start-1].c->status;
+}
+
---
+++ lab1-Skeleton/testc.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+# UCLA CS 111 Lab 1 - Test that valid syntax is processed correctly.
+
+tmp=$0-$$.tmp
+mkdir "$tmp" || exit
+
+(
+cd "$tmp" || exit
+
+cat >test.sh <<'EOF'
+
+#this stuff should work
+
+echo hello world > hello.tmp
+cat hello.tmp | grep hello && cat hello.tmp
+echo hellogoodbye | grep hello && echo hello && echo hi || echo goodbye
+( echo this is a subshell; echo it uses semicolons; (echo this is a nested subshell && echo it does complex commands) )
+( echo one; 
+  echo two && (
+  echo three && 
+  echo four) )
+
+(cat hello.tmp; echo hello world) | grep hello
+
+echo hello again
+
+(ls .. | (grep test | ( ls .. && cat | sort) ) ) && (echo complicated things && (ls .. | sort) )
+
+echo seven
+
+(echo one | exec sort; echo two) |
+(ls .. | exec sort | exec cat &&
+exec ls .. | exec grep st > a.tmp; echo four)
+
+EOF
+
+chmod 777 test.sh
+./test.sh | sort > test.exp
+../timetrash -t test.sh | sort > test.out 2>test.err || exit
+
+diff -u test.exp test.out || exit
+test ! -s test.err || {
+  cat test.err
+  exit 1
+}
+
+) || exit
+
+rm -fr "$tmp"
